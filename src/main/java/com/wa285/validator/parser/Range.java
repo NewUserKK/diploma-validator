3e2fe88e40000b2981c1cf9c72303e4431892f25
CaelmBleidd <alex.menshutin99@gmail.com>
2018-10-27 20:02:56 +0300

    Add Range-class

---
+++ src/main/java/com/wa285/validator/parser/Range.java
@@ -0,0 +1,26 @@
+package com.wa285.validator.parser;
+
+public class Range
+{
+    private int low;
+    private int high;
+    private int center;
+
+    private static final int range = 15;
+
+    public Range(int low, int high){
+        this.low = low;
+        this.high = high;
+        this.center = (low + high) / 2;
+    }
+
+    public Range(int center) {
+        this.low = center - range;
+        this.high = center + range;
+        this.center = center;
+    }
+
+    public boolean contains(int number){
+        return (number >= low && number <= high);
+    }
+}
