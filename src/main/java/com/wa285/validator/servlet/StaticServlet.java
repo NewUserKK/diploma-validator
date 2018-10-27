3b5db3f6917c671b284a64e9efdf832db6c504aa
damtev <damtev@yandex.ru>
2018-10-27 17:57:42 +0300

    Uploading files code

--- src/main/java/com/wa285/validator/servlet/StaticServlet.java
+++ src/main/java/com/wa285/validator/servlet/StaticServlet.java
@@ -1,5 +1,9 @@
 package com.wa285.validator.servlet;
 
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -8,6 +12,9 @@ import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.file.Files;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
 
 
 public class StaticServlet extends HttpServlet {
@@ -42,9 +49,86 @@ public class StaticServlet extends HttpServlet {
         outputStream.flush();
     }
 
+    private Random random = new Random();
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        super.doPost(request, response);
+        /*//проверяем является ли полученный запрос multipart/form-data
+        boolean isMultipart = ServletFileUpload.isMultipartContent(request);
+        if (!isMultipart) {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }*/
+
+        // Создаём класс фабрику
+        DiskFileItemFactory factory = new DiskFileItemFactory();
+
+        // Максимальный буфера данных в байтах,
+        // при его привышении данные начнут записываться на диск во временную директорию
+        // устанавливаем один мегабайт
+        factory.setSizeThreshold(1024 * 1024);
+
+        // устанавливаем временную директорию
+        File tempDir = (File) getServletContext().getAttribute("javax.servlet.context.tempdir");
+        factory.setRepository(tempDir);
+
+        //Создаём сам загрузчик
+        ServletFileUpload upload = new ServletFileUpload(factory);
+
+        //максимальный размер данных который разрешено загружать в байтах
+        //по умолчанию -1, без ограничений. Устанавливаем 10 мегабайт.
+        upload.setSizeMax(1024 * 1024 * 10);
+
+        try {
+            List items = upload.parseRequest(request);
+            Iterator iter = items.iterator();
+
+            while (iter.hasNext()) {
+                FileItem item = (FileItem) iter.next();
+
+                if (item.isFormField()) {
+                    //если принимаемая часть данных является полем формы
+                    processFormField(item);
+                } else {
+                    //в противном случае рассматриваем как файл
+                    processUploadedFile(item);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            return;
+        }
+    }
+
+    /**
+     * Сохраняет файл на сервере, в папке upload.
+     * Сама папка должна быть уже создана.
+     *
+     * @param item
+     * @throws Exception
+     */
+    private void processUploadedFile(FileItem item) throws Exception {
+        File uploadetFile = null;
+        //выбираем файлу имя пока не найдём свободное
+        do {
+            String path = getServletContext().getRealPath("/upload/" + random.nextInt() + item.getName());
+            uploadetFile = new File(path);
+        } while (uploadetFile.exists());
+
+        //создаём файл
+        uploadetFile.createNewFile();
+        //записываем в него данные
+        item.write(uploadetFile);
+    }
+
+    /**
+     * Выводит на консоль имя параметра и значение
+     *
+     * @param item
+     */
+    private void processFormField(FileItem item) {
+        System.out.println(item.getFieldName() + "=" + item.getString());
     }
 
     private String getContentTypeFromName(String name) {
