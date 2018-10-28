131ec185857a7c9d6388bcc1b0d8f3cb34683c2f
erlnby <zolotoie@gmail.com>
2018-10-28 06:01:52 +0300

    Add DocumentFormatError method fix

--- src/main/java/com/wa285/validator/parser/errors/critical/DocumentFormatError.java
+++ src/main/java/com/wa285/validator/parser/errors/critical/DocumentFormatError.java
@@ -3,6 +3,10 @@ package com.wa285.validator.parser.errors.critical;
 import com.wa285.validator.parser.errors.Location;
 import org.apache.poi.xwpf.usermodel.XWPFRun;
 
+import java.math.BigInteger;
+
+import static com.wa285.validator.parser.ElementSize.*;
+
 public class DocumentFormatError extends Critical {
 
     public DocumentFormatError(String description, Location location) {
@@ -11,6 +15,8 @@ public class DocumentFormatError extends Critical {
 
     @Override
     public void fix(XWPFRun run) {
-
+        var PgSz = run.getDocument().getDocument().getBody().getSectPr().addNewPgSz();
+        PgSz.setH(BigInteger.valueOf(DOCUMENT_HEIGHT.value().getCenter()));
+        PgSz.setW(BigInteger.valueOf(DOCUMENT_WIDTH.value().getCenter()));
     }
 }
